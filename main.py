14df8ab90a3fcad4d9b0b8dc795d41c450d8dff2
Elyson <elysonpanolino@gmail.com>
2026-03-15 02:23:32 +0800

    main-fastapi boiler plate

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from my-api!")
+import os
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"status": "active", "message": "FastAPI on Render"}
 
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
